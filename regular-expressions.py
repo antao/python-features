be7160d7c7da9bb0692f2db80a7e6b5aa001af04
João Antão <joao.antao@protonmail.com>
2016-11-22 23:23:39 +0100

    Add regular-expressions.py (re module example)

---
+++ regular-expressions.py
@@ -0,0 +1,12 @@
+import re
+
+if __name__ == '__main__':
+    sentence= "a fat cat doesn't eat oat but a rat eats bats."
+    
+    findall = re.findall("[force]at", sentence)
+    print(findall)
+    # search = re.search("fat.*(animal|dog|zebra|cat)", sentence)
+    
+    # if search:
+    #     print (search.group())
+    #     print (search)
